7bd76ca37790fe310874463410a632b0e5f74d07
seungik <seungik@seungikui-MacBookPro.local>
2025-08-04 22:44:32 +0900

    fix: rn wepin setting

---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+  dependencies: {
+    '@wepin/storage-rn': {
+      root: path.join(__dirname, './node_modules/@wepin/storage-rn'),
+    },
+  },
+};
